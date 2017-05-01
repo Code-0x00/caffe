b9381e15c1b9c8de0470a568ef1c47d647f58011
E450 <yohoho233@gmail.com>
2017-05-01 18:50:18 -0400

    make all ok

--- LinuxYumInstall/CentOS7.sh
+++ LinuxYumInstall/CentOS7.sh
@@ -1,1 +1,1 @@
-yum install -y epel-release python-pip protobuf-devel leveldb-devel snappy-devel opencv-devel boost-devel hdf5-devel gflags-devel glog-devel lmdb-devel
+yum install -y gcc gcc-c++ epel-release python-pip protobuf-devel leveldb-devel snappy-devel opencv-devel boost-devel hdf5-devel gflags-devel glog-devel lmdb-devel openblas-devel
